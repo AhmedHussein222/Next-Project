3a3aed3fd9fece88fefe007f539612f5fd29f9ca
AhmedHussein222 <ahmdhussein222@gmail.com>
2025-05-04 09:15:12 +0300

    fix: Correct JSX syntax and improve accessibility in product links

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -32,11 +32,10 @@ async function Users() {
               key={product.id}
               className="relative  flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md"
             >
-              <a
+         
+                <Link href={`/products/${product.id}`}
                 className="relative mx-3 mt-3 p-4 flex h-60 overflow-hidden rounded-xl"
-                href="#"
-              >
-                <Link href={`/products/${product.id}`}>
+                >
                   <Image
                     className="object-fill"
                     src={product.image}
@@ -44,11 +43,12 @@ async function Users() {
                     width={250} // Set appropriate width
                     height={400}
                   />
-                </Link>
-                <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">
+                   <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">
                   39% OFF
                 </span>
-              </a>
+                </Link>
+               
+             
               <div className="mt-4 px-5 pb-5">
                 <a href="#">
                   <h5 className="text-xl h-7 tracking-tight text-slate-900 overflow-hidden">
@@ -125,11 +125,11 @@ async function Users() {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    stroke-width="2"
+                    strokeWidth="2"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                     strokeLinecap="round"
+                     strokeLinejoin="round"
                       d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                     />
                   </svg>
